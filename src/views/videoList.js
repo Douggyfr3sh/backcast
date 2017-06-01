a6d2e16d3aef0919d73179b7693c52c16067f0be
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-06-01 15:05:13 -0400

    Tests passing for VideoListView, VideoListEntryView.
    Basic tests now pass for AppView (moved sample data array locally to AppView).  Our video list is not rendering on the index.html page (need to get that to happen next).

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,22 +1,27 @@
 var VideoListView = Backbone.View.extend({
 
   initialize: function() {
-    //console.log('this inside VideoListView: ', this);
-    // this.collection.on('sync', function() {
-    //   this.render();
-    // }, this);
+    this.listenTo(this.collection, 'sync', this.render);
   },
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
 
-    // console.log('collection', this.collection);
+    //console.log('collection', this.collection);
+
+
+    //selecting video list from DOM and appending each instance of the collection as a new VideoListEntry
+    this.$('.video-list').append(this.collection.map(function(video) { 
+      console.log('video inside map ',video);
+      return new VideoListEntryView({ model: video }).render().el; 
+    }));
+
     // for (var i = 0; i < this.collection.length; i++) {
     //   var videoEntry = new VideoListEntryView({model: this.collection[i]});
     //   videoEntry.render();
-      // console.log('videoentry',videoEntry);
-    //}
+    //   console.log('videoentry',videoEntry);
+    // }
 
     return this;
   },
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -3,8 +3,18 @@ var VideoListEntryView = Backbone.View.extend({
 
   render: function() {
     console.log('this VideoListEntryView', this);
-    this.$el.html(this.template());
+    this.$el.html(this.template(this.model.attributes));
+    //Refactor to dynamically render based on the video object it receives
+
     return this;
+  },
+
+  handleClick: function () {
+    this.model.select();
+  },
+
+  events: {
+    'click .video-list-entry-title' : 'handleClick'
   },
 
   template: templateURL('src/templates/videoListEntry.html')
